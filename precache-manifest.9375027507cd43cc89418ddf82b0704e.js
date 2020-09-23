05743158e4424ba88b101393235d11c6bf22a780
SKNAUSAD97 <sknausat97@gmail.com>
2020-09-23 15:49:05 +0530

    Updates

---
+++ precache-manifest.9375027507cd43cc89418ddf82b0704e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a524138b5547c7dc70d181406c7c2887",
+    "url": "/VISITORS-MANAGEMENT-SYSTEM/index.html"
+  },
+  {
+    "revision": "ffe19473e872f39e1469",
+    "url": "/VISITORS-MANAGEMENT-SYSTEM/static/js/2.754aab99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VISITORS-MANAGEMENT-SYSTEM/static/js/2.754aab99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c261800420b6d36ab274",
+    "url": "/VISITORS-MANAGEMENT-SYSTEM/static/js/main.7f3c4a99.chunk.js"
+  },
+  {
+    "revision": "da2a3929921482538c00",
+    "url": "/VISITORS-MANAGEMENT-SYSTEM/static/js/runtime-main.93f8c835.js"
+  }
+]);
